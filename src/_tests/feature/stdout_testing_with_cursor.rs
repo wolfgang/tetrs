a41303e74d2f4285d75ae5c8f41cb313588d8b01
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-07-27 14:54:48 +0200

    Fix some names

--- src/_tests/feature/stdout_testing_with_cursor.rs
+++ src/_tests/feature/stdout_testing_with_cursor.rs
@@ -1,37 +1,37 @@
 use std::io::{Cursor, Write};
 use std::str;
 
-type LineBuffer = Cursor<Vec<u8>>;
-type Lines<'a> = Vec<&'a str>;
+type OutputBuffer = Cursor<Vec<u8>>;
+type Output<'a> = Vec<&'a str>;
 
 #[test]
 fn nothing() {
     let buffer = render("line1 1234\nline2 23233");
-    assert_lines_start_with(&buffer, vec!["line1", "line2"])
+    assert_output(&buffer, vec!["line1", "line2"])
 }
 
 
-pub fn render(text: &str) -> LineBuffer {
+pub fn render(text: &str) -> OutputBuffer {
     let mut buffer = Cursor::new(Vec::new());
     buffer.write(text.as_bytes()).unwrap();
     buffer
 }
 
 
-pub fn assert_lines_start_with(buffer: &LineBuffer, expected_lines: Lines) {
-    let rendered_lines = lines_from(buffer);
-    assert_eq!(rendered_lines.len(), expected_lines.len(), "Number of rendered lines not as expected");
+pub fn assert_output(buffer: &OutputBuffer, expected_output: Output) {
+    let rendered_output = output_from(buffer);
+    assert_eq!(rendered_output.len(), expected_output.len(), "Number of rendered lines not as expected");
 
-    let line_length = expected_lines[0].len();
-    let actual_lines: Vec<String> = rendered_lines
+    let line_length = expected_output[0].len();
+    let actual_lines: Vec<String> = rendered_output
         .iter()
         .map(|s| s[0..line_length].to_string())
         .collect();
 
-    assert_eq!(actual_lines, expected_lines);
+    assert_eq!(actual_lines, expected_output);
 }
 
-fn lines_from(buffer: &LineBuffer) -> Lines {
+fn output_from(buffer: &OutputBuffer) -> Output {
     let actual_string = str::from_utf8(buffer.get_ref()).unwrap();
     let lines: Vec<&str> = actual_string.split("\n").collect();
     lines.to_vec()
