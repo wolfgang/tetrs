3562d38fb389f78eb27bc2fff725d4f11142d623
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-07-28 12:41:07 +0200

    Clear terminal on start

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[allow(unreachable_code)]
 fn main() -> std::io::Result<()> {
-    let mut stdout = Term::stdout();
 
     let field_height = 16;
     let mut game = Game::init()
@@ -14,16 +13,20 @@ fn main() -> std::io::Result<()> {
         .build();
 
 
-    let mut rendered_lines = field_height + 1;
+    let  rendered_lines = field_height + 1;
 
-    let mut last_render_millis = get_now_millis();
+    let mut stdout = Term::stdout();
+    stdout.clear_screen()?;
+
+    let mut last_frame_millis = get_now_millis();
+
     loop {
         let now_millis = get_now_millis();
-        if now_millis - last_render_millis >= 100 {
+        if now_millis - last_frame_millis >= 100 {
             game.tick(get_now_millis());
-            last_render_millis = now_millis;
+            last_frame_millis = now_millis;
             stdout.clear_last_lines(rendered_lines as usize)?;
-            rendered_lines = game.render(&mut stdout).unwrap();
+            game.render(&mut stdout).unwrap();
         }
     }
 
