da64e538db75a0a913b862ee764ed13d1a0a986d
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-07-28 08:41:14 +0200

    Add Game::field_height

--- src/game.rs
+++ src/game.rs
@@ -3,16 +3,17 @@ use std::io::Write;
 pub struct Game {
     last_drop_millis: u64,
     brick_row: u8,
+    field_height: u8
 }
 
 impl Game {
     pub fn new() -> Game {
-        Game { brick_row: 0, last_drop_millis: 0 }
+        Game { brick_row: 0, last_drop_millis: 0, field_height: 16 }
     }
 
     pub fn tick(&mut self, new_time_millis: u64) {
         while new_time_millis - self.last_drop_millis  >= 100 {
-            if self.brick_row < 15 {
+            if self.brick_row < self.field_height - 1 {
                 self.brick_row += 1;
             }
             self.last_drop_millis = self.last_drop_millis + 100;
@@ -20,7 +21,7 @@ impl Game {
     }
 
     pub fn render(&self, writer: &mut dyn Write) -> std::io::Result<()> {
-        for row in 0..16 {
+        for row in 0..self.field_height {
             if row == self.brick_row {
                 writer.write(b"| ####     |\n")?;
             } else {
