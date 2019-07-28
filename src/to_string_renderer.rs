126b7dea442550ba2d324827ef8f4f78a7a81127
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-07-28 14:56:47 +0200

    Clean up

--- src/to_string_renderer.rs
+++ src/to_string_renderer.rs
@@ -1,29 +1,28 @@
 pub struct ToStringRenderer {
-    pub frame_buffer: Vec<String>,
-    width: u8,
+    pub frame_buffer: Vec<Vec<char>>,
+    width: usize,
 }
 
 impl ToStringRenderer {
-    pub fn new(width: u8, height: u8) -> ToStringRenderer {
-        let mut frame_buffer: Vec<String> = Vec::with_capacity(height as usize);
-
-        for _ in 0..height as usize {
-            frame_buffer.push(String::with_capacity(width as usize))
-        }
+    pub fn new(width: usize, height: usize) -> ToStringRenderer {
+        let frame_buffer = vec![Vec::with_capacity(width); height ];
 
         ToStringRenderer {
             frame_buffer,
-            width
+            width,
         }
     }
 
     pub fn clear(&mut self) {
         for i in 0..self.frame_buffer.len() {
-            self.frame_buffer[i] = ".".repeat(self.width as usize)
+            self.frame_buffer[i] = vec!['.'; self.width];
         }
     }
 
     pub fn frame_buffer_strings(&self) -> Vec<String> {
-        self.frame_buffer.clone()
+        self.frame_buffer
+            .iter()
+            .map(|row| { row.iter().collect() })
+            .collect()
     }
 }
