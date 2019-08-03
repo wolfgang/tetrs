f4599a35c0675896aa9e8429ad9ae04cec466117
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-08-03 14:30:16 +0200

    Rename tests

--- src/_tests/feature/mod.rs
+++ src/_tests/feature/mod.rs
@@ -1,6 +1,6 @@
-pub mod game_tick_test;
-pub mod game_input_test;
-pub mod game_vertical_collision_test;
+pub mod brick_drop_test;
+pub mod horizontal_movement_test;
+pub mod vertical_collision_test;
 pub mod horizontal_collision_test;
 pub mod sequential_brick_provider_test;
 pub mod testable_game_factory_test;
