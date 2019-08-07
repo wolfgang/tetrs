1353c138b12f9d4a93ad928d18adc3446c072682
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-08-07 10:21:34 +0200

    Move the rest of the code from Game to GameState

--- src/game.rs
+++ src/game.rs
@@ -8,7 +8,6 @@ pub mod renderer;
 pub mod state;
 
 use trenderer::TRenderer;
-use brick_provider::BrickProviderRef;
 use brick::Brick;
 use builder::GameBuilder;
 use renderer::GameRenderer;
@@ -16,7 +15,6 @@ use state::GameState;
 
 
 pub struct Game {
-    brick_provider: BrickProviderRef,
     renderer: GameRenderer,
     state: GameState,
 }
@@ -33,10 +31,10 @@ impl Game {
         }
 
         Self {
-            brick_provider: builder.brick_provider.clone(),
             renderer: GameRenderer {},
             state: GameState {
                 input: builder.input.clone(),
+                brick_provider: builder.brick_provider.clone(),
                 drop_interval: builder.drop_interval,
                 last_drop_millis: builder.current_time_millis,
                 last_move_millis: 0,
@@ -49,60 +47,13 @@ impl Game {
     }
 
     pub fn tick(&mut self, now_millis: u64) {
-        self.rotate_brick(now_millis);
-        self.move_brick_horizontally(now_millis);
-        self.drop_brick(now_millis)
+        self.state.rotate_brick(now_millis);
+        self.state.move_brick_horizontally(now_millis);
+        self.state.drop_brick(now_millis)
     }
 
     pub fn render(&self, t_renderer: &mut dyn TRenderer) {
         self.renderer.render(t_renderer, &self.state);
     }
 
-    fn rotate_brick(&mut self, now_millis: u64) {
-        if now_millis - self.state.last_rotation_millis >= 150 &&
-            self.state.input.borrow().wants_to_rotate() &&
-            self.can_rotate()
-        {
-            self.state.last_rotation_millis = now_millis;
-            self.state.active_brick.goto_next_phase();
-        }
-    }
-
-    fn move_brick_horizontally(&mut self, now_millis: u64) {
-        let speed = self.get_horizontal_move_speed(now_millis);
-        if speed != 0 {
-            self.state.last_move_millis = now_millis;
-            self.state.active_brick.move_by(speed, 0);
-        }
-    }
-
-    fn drop_brick(&mut self, now_millis: u64) -> () {
-        if self.state.is_time_to_drop(now_millis) {
-            if self.state.can_drop() {
-                self.state.active_brick.move_by(0, 1);
-                self.state.last_drop_millis = now_millis;
-            } else {
-                self.state.last_drop_millis = now_millis;
-
-                for (x, y) in self.state.active_brick.current_bricklets() {
-                    self.state.field[y][x] = self.state.active_brick.brick_type();
-                }
-
-                self.state.active_brick.reset(self.brick_provider.borrow_mut().next());
-            }
-        }
-    }
-
-    fn can_rotate(&self) -> bool {
-        self.state.active_brick.all_next_bricklets(|x, y| {
-            self.state.is_empty_cell(x as i32, y)
-        })
-    }
-
-    fn get_horizontal_move_speed(&self, now_millis: u64) -> i8 {
-        if !self.state.is_time_to_move(now_millis) { return 0; };
-        if self.state.input.borrow().wants_to_move_right() && self.state.can_move_to(1) { return 1; }
-        if self.state.input.borrow().wants_to_move_left() && self.state.can_move_to(-1) { return -1; }
-        return 0;
-    }
 }
--- src/game/state.rs
+++ src/game/state.rs
@@ -1,5 +1,6 @@
 use crate::game::brick::Brick;
 use crate::game::tinput::TInputRef;
+use crate::game::brick_provider::BrickProviderRef;
 
 const FIELD_WIDTH: usize = 10;
 
@@ -11,29 +12,79 @@ pub struct GameState {
     pub(super) active_brick: Brick,
     pub(super) drop_interval: u16,
     pub(super) input: TInputRef,
+    pub(super) brick_provider: BrickProviderRef
 }
 
 impl GameState {
-    pub(super) fn can_drop(&self) -> bool {
+    pub(super) fn rotate_brick(&mut self, now_millis: u64) {
+        if now_millis - self.last_rotation_millis >= 150 &&
+            self.input.borrow().wants_to_rotate() &&
+            self.can_rotate()
+        {
+            self.last_rotation_millis = now_millis;
+            self.active_brick.goto_next_phase();
+        }
+    }
+
+    pub(super) fn move_brick_horizontally(&mut self, now_millis: u64) {
+        let speed = self.get_horizontal_move_speed(now_millis);
+        if speed != 0 {
+            self.last_move_millis = now_millis;
+            self.active_brick.move_by(speed, 0);
+        }
+    }
+
+    pub(super) fn drop_brick(&mut self, now_millis: u64) -> () {
+        if self.is_time_to_drop(now_millis) {
+            if self.can_drop() {
+                self.active_brick.move_by(0, 1);
+                self.last_drop_millis = now_millis;
+            } else {
+                self.last_drop_millis = now_millis;
+
+                for (x, y) in self.active_brick.current_bricklets() {
+                    self.field[y][x] = self.active_brick.brick_type();
+                }
+
+                self.active_brick.reset(self.brick_provider.borrow_mut().next());
+            }
+        }
+    }
+
+
+    fn can_rotate(&self) -> bool {
+        self.active_brick.all_next_bricklets(|x, y| {
+            self.is_empty_cell(x as i32, y)
+        })
+    }
+
+    fn get_horizontal_move_speed(&self, now_millis: u64) -> i8 {
+        if !self.is_time_to_move(now_millis) { return 0; };
+        if self.input.borrow().wants_to_move_right() && self.can_move_to(1) { return 1; }
+        if self.input.borrow().wants_to_move_left() && self.can_move_to(-1) { return -1; }
+        return 0;
+    }
+
+    fn can_drop(&self) -> bool {
         self.active_brick.all_bricklets(|x, y| {
             self.is_empty_cell(x as i32, y + 1)
         })
     }
 
-    pub(super) fn is_time_to_drop(&self, now_millis: u64) -> bool {
+    fn is_time_to_drop(&self, now_millis: u64) -> bool {
         now_millis - self.last_drop_millis >= self.drop_interval as u64
     }
 
-    pub(super) fn is_time_to_move(&self, now_millis: u64) -> bool {
+    fn is_time_to_move(&self, now_millis: u64) -> bool {
         self.active_brick.is_above(self.field.len() as u8 - 1) &&
             now_millis - self.last_move_millis >= 50
     }
 
-    pub(super) fn can_move_to(&self, offset: i32) -> bool {
+    fn can_move_to(&self, offset: i32) -> bool {
         self.active_brick.all_bricklets(|x, y| { self.is_empty_cell(x as i32 + offset, y) })
     }
 
-    pub(super) fn is_empty_cell(&self, x: i32, y: usize) -> bool {
+    fn is_empty_cell(&self, x: i32, y: usize) -> bool {
         x >= 0
             && (x as usize) < FIELD_WIDTH
             && y < self.field.len()
