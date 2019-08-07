786f3d39ff148c6db2e4ecf6577e4cd106baf5f3
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-08-07 11:56:58 +0200

    Verify that two lines can vanish at once

--- src/_tests/feature/vanishing_lines_test.rs
+++ src/_tests/feature/vanishing_lines_test.rs
@@ -72,3 +72,55 @@ fn lines_above_vanished_lines_drop_down() {
     ]);
 
 }
+
+
+#[test]
+fn two_vanishing_lines() {
+    let mut game = TestableGame::init()
+        .with_brick_sequence(vec![i_block(), i_block()])
+        .with_field(vec![
+            "........##",
+            "..........",
+            "###.######",
+            "#....#....",
+            "###.######",
+        ])
+        .with_drop_interval(200)
+        .build();
+
+    game.verify_frame_after(0, vec![
+        ".####...##",
+        "..........",
+        "###.######",
+        "#....#....",
+        "###.######",
+    ]);
+
+    game.is_rotating();
+
+    game.verify_frame_after(150, vec![
+        "...#....##",
+        "...#......",
+        "##########",
+        "#..#.#....",
+        "###.######",
+    ]);
+
+
+    game.verify_frame_after(200, vec![
+        "........##",
+        "...#......",
+        "##########",
+        "#..#.#....",
+        "##########",
+    ]);
+
+    game.verify_frame_after(400, vec![
+        ".####.....",
+        "..........",
+        "........##",
+        "...#......",
+        "#..#.#...."
+    ]);
+
+}
