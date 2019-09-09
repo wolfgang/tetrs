219d873fcfb9110566781299ffdd8d42415dca79
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-09-09 14:19:18 +0200

    Explicitly configure parameters

--- src/_tests/feature/demo_tests.rs
+++ src/_tests/feature/demo_tests.rs
@@ -1,8 +1,13 @@
+use crate::game::brick_factory::*;
 use crate::_tests::helpers::testable_game::TestableGame;
 
 #[test]
 fn after_brick_reaches_bottom_it_stays_there() {
-    let mut game = TestableGame::init().with_field_height(3).build();
+    let mut game = TestableGame::init()
+        .with_drop_interval(100)
+        .with_brick_sequence(vec![i_block(), i_block()])
+        .with_field_height(3)
+        .build();
 
     game.verify_exact_frame_after(100, vec![
         "..........",
